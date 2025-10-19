719bb25470bcf35fb9d79f6fe3040d64428f8e21
Nikita <nyuskov@gmail.com>
2025-10-19 17:17:31 +0300

    add templates and translations for admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
+
 from .models import Post
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'publish')
 
 admin.site.register(Post, PostAdmin)
+admin.site.site_header = _('Project administration')
+admin.site.index_title = _('Site administration')
--- blog/apps.py
+++ blog/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.contrib.admin import AdminSite
 from django.utils.translation import gettext_lazy as _
 
 
