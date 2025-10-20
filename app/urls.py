24acb49be3032903554c83674220426ef129449c
Nikita <nyuskov@gmail.com>
2025-10-20 13:36:56 +0300

    add new post views

--- app/urls.py
+++ app/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.urls import path, include
 from django.utils.translation import gettext as _
+from django.views.generic import RedirectView
+
 
 # Изменение административной части сайта
 admin.site.site_header = _('Project administration')
@@ -26,4 +29,10 @@ admin.site.index_title = _('Site administration')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico')),
+    ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseNotFound
 
-# Create your views here.
+from .models import Post
+
+
+def  post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
