a765b56062929db212dfa00d033b63d927714b75
Nikita <nyuskov@gmail.com>
2025-10-23 11:38:01 +0300

    add change and reset password

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,14 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
+from django.urls import path, reverse_lazy
 
 from .views import dashboard
 
@@ -9,5 +18,43 @@ app_name = "account"
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    # password change
+    path(
+        "password-change/",
+        PasswordChangeView.as_view(
+            success_url=reverse_lazy("account:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    # password reset
+    path(
+        "password-reset/",
+        PasswordResetView.as_view(
+            success_url=reverse_lazy("account:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy("account:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("", dashboard, name="dashboard"),
 ]
