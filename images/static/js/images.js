bfb252313e9c7afd4be88917cc3a5ad1e332efdf
Nikita <nyuskov@gmail.com>
2025-10-27 12:44:32 +0300

    fix bugs

--- images/static/js/images.js
+++ images/static/js/images.js
@@ -36,5 +36,30 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         });
     });
+  } else {
+    let page = 1;
+    let emptyPage = false;
+    let blockRequest = false;
+    window.addEventListener("scroll", function (e) {
+      let margin = document.body.clientHeight - window.innerHeight - 200;
+      if (window.pageYOffset > margin && !emptyPage && !blockRequest) {
+        blockRequest = true;
+        page += 1;
+        fetch("?images_only=1&page=" + page)
+          .then((response) => response.text())
+          .then((html) => {
+            if (html === "") {
+              emptyPage = true;
+            } else {
+              let imageList = document.getElementById("image-list");
+              imageList.insertAdjacentHTML("beforeEnd", html);
+              blockRequest = false;
+            }
+          });
+      }
+    });
+    // Launch scroll event
+    const scrollEvent = new Event("scroll");
+    window.dispatchEvent(scrollEvent);
   }
 });
--- images/urls.py
+++ images/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ImageCreateView, ImageDetailView, image_like
+from .views import ImageCreateView, ImageDetailView, image_like, image_list
 
 app_name = "images"
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
         "detail/<int:id>/<slug:slug>/", ImageDetailView.as_view(), name="detail"
     ),
     path("like/", image_like, name="like"),
+    path("", image_list, name="list"),
 ]
