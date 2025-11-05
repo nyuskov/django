8ed2f07b2d932a1ff131d50f335318d5bd6aeabe
Nikita <nyuskov@gmail.com>
2025-11-05 16:46:35 +0300

    add admin features

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -13,6 +14,8 @@ class Order(models.Model):
     address: models.CharField = models.CharField(
         max_length=250, verbose_name=_("Address")
     )
+    stripe_id: models.CharField = models.CharField(
+        max_length=250, blank=True, verbose_name=_("Stripe ID"))
     postal_code: models.CharField = models.CharField(
         max_length=20, verbose_name=_("Postal code")
     )
@@ -42,6 +45,18 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            # no payment associated
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            # Stripe path for test payments
+            path = '/test/'
+        else:
+            # Stripe path for real payments
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
 
 
 class OrderItem(models.Model):
--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import payment_canceled, payment_completed, payment_process
+from .webhooks import stripe_webhook
 
 app_name = "payment"
 urlpatterns = [
     path("process/", payment_process, name="process"),
     path("completed/", payment_completed, name="completed"),
     path("canceled/", payment_canceled, name="canceled"),
+    path("webhook/", stripe_webhook, name="stripe-webhook"),
 ]
