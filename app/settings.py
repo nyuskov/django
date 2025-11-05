210e0595a97930f9e6393e05c94ae1443b1feddf
Nikita <nyuskov@gmail.com>
2025-11-05 08:23:43 +0300

    add stripe

--- app/settings.py
+++ app/settings.py
@@ -48,11 +48,18 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
+# SMS
 SMS_ENABLED = True
 SMS_API_KEY = config("SMS_API_KEY")
 
+# Telegram
 TELEGRAM_ENABLED = True
 TELEGRAM_BOT_TOKEN = config("TELEGRAM_BOT_TOKEN")
+
+# Stripe payments
+STRIPE_PUBLISHABLE_KEY = config("STRIPE_PUBLISHABLE_KEY")
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
+STRIPE_API_VERSION = "2025-10-29"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
